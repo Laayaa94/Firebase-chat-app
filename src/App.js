02994440ce49cedf2b20878c074077dbaa4f5804
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-06 12:39:15 +0530

    Added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Chat from './Components/chat/Chat';
+import Detail from './Components/detail/Detail';
+import List from './Components/list/List';
 
 function App() {
   return (
     <div className="ChatContainer">
-    
+      <List/>
+      <Chat/>
+      <Detail/>
     </div>
   );
 }
