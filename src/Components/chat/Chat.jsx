632e9a91791a16480882db2987c0a18f14da2a64
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-06 16:48:07 +0530

    chat container bottom part

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Chat.css'
+import EmojiPicker from 'emoji-picker-react'
+import img from '../../Assets/img.png'
+import camera from '../../Assets/camera.png'
+import microphone from '../../Assets/mic.png'
+import emoji from '../../Assets/emoji.png'
 import phone from '../../Assets/phone.png'
 import video from '../../Assets/video.png'
 import info from '../../Assets/info.png'
 import avatar from '../../Assets/avatar.png'
 const Chat = () => {
+  
   return (
     <div className='chat'>
       <div className="top">
@@ -26,9 +32,16 @@ const Chat = () => {
       </div>
       <div className="bottom">
       <div className="icons">
-        <input type="text" placeholder='Type a message...'/>
-        <div className="emogi"></div>
+        <img src={img} alt="" />
+        <img src={camera} alt="" />
+        <img src={microphone} alt="" />
       </div>
+      <input type="text" placeholder='Type a message...'/>
+      <div className="emogi">
+          <img src={emoji} alt="" />
+          <EmojiPicker/>
+        </div>
+        <button className='sendbtn'>Send</button>
       </div>
     </div>
   )
