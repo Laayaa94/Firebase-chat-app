6a8cf1c299d1efb04a35d7bad63b4ee2c6c98237
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-06 14:13:22 +0530

    chatlist

--- src/Components/list/ChatList/Chatlist.jsx
+++ src/Components/list/ChatList/Chatlist.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Chatlist.css'
+import search from '../../../Assets/search.png'
+import plus from '../../../Assets/plus.png'
+import minus from '../../../Assets/minus.png'
+import avatar from '../../../Assets/avatar.png'
+
 const Chatlist = () => {
+  const [addMode,setAddmode]=useState(false);
   return (
-    <div className='ChatListContainer'>
-      
+    <div className='ChatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src={search} alt="" />
+          <input type="text" placeholder='Search'/>
+        </div>
+        <img src={ addMode ? minus : plus} alt="" className='add'  onClick={()=>setAddmode((prev)=>!prev)} />
+      </div>
+
+      <div className="item">
+        <img src={avatar} alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={avatar} alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={avatar} alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/list/UserInfo/UserInfo.jsx
+++ src/Components/list/UserInfo/UserInfo.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './UserInfo.css'
+import more from '../../../Assets/more.png'
+import video from '../../../Assets/video.png'
+import edit from '../../../Assets/edit.png'
+import avatar from '../../../Assets/avatar.png'
+
+
 const UserInfo = () => {
   return (
-    <div className=''>
-      userinfo
+    <div className='UserInfo'>
+      <div className="user">
+        <img src={avatar} alt="" />
+        <h2>Jane Doe</h2>
+      </div>
+      <div className="icons">
+        <img src={more} alt="" />
+        <img src={video} alt="" />
+        <img src={edit} alt="" />
+      </div>
     </div>
   )
 }
